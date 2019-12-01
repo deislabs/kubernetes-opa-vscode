b970748755a6af030e08d8df4110cbf099f8e23f
Ivan Towlson <itowlson@microsoft.com>
2019-12-02 11:33:53 +1300

    Deployment command

--- src/extension.ts
+++ src/extension.ts
@@ -4,11 +4,16 @@ import { install } from './commands/install';
 export function activate(context: vscode.ExtensionContext) {
 
     const disposables = [
-        vscode.commands.registerCommand('opak8s.install', install)
+        vscode.commands.registerCommand('opak8s.install', install),
+        vscode.commands.registerTextEditorCommand('opak8s.deployRego', deployRego),
     ];
 
     context.subscriptions.push(...disposables);
 }
 
 export function deactivate() {
+}
+
+function deployRego(textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit) {
+    vscode.window.showInformationMessage("quick everybody look like you're deploying");
 }
