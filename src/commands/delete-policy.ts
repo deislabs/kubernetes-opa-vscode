9cc81e92631225e9dd304cfebd1c446af1c41f8c
itowlson <itowlson@microsoft.com>
2019-12-04 10:26:31 +1300

    Delete policy command (#14)

---
+++ src/commands/delete-policy.ts
@@ -0,0 +1,44 @@
+import * as vscode from 'vscode';
+import * as k8s from 'vscode-kubernetes-tools-api';
+import { unavailableMessage, confirm, longRunning } from '../utils/host';
+import { PolicyBrowser } from '../ui/policy-browser';
+import { policyIsDevRego, ConfigMap, OPA_NAMESPACE } from '../opa';
+
+export async function deletePolicy(target: any) {
+    const clusterExplorer = await k8s.extension.clusterExplorer.v1;
+    const kubectl = await k8s.extension.kubectl.v1;
+    if (!clusterExplorer.available) {
+        await vscode.window.showWarningMessage(`Can't run command: ${unavailableMessage(clusterExplorer.reason)}`);
+        return;
+    } else if (!kubectl.available) {
+        await vscode.window.showWarningMessage(`Can't run command: ${unavailableMessage(kubectl.reason)}`);
+        return;
+    }
+
+    const node = PolicyBrowser.resolve(target, clusterExplorer.api);
+    if (node && node.nodeType === 'policy') {
+        const policy = node.configmap;
+        await tryDeletePolicy(policy, clusterExplorer.api, kubectl.api);
+    }
+}
+
+async function tryDeletePolicy(policy: ConfigMap, clusterExplorer: k8s.ClusterExplorerV1, kubectl: k8s.KubectlV1): Promise<void> {
+    if (!policyIsDevRego(policy)) {
+        const confirmed = await confirm(`Config map ${policy.metadata.name} was not deployed by the OPA extension.`, 'Delete it Anyway');
+        if (!confirmed) {
+            return;
+        }
+    }
+
+    const deleteResult = await longRunning(`Deleting config map ${policy.metadata.name}...`, () =>
+        kubectl.invokeCommand(`delete configmap ${policy.metadata.name} --namespace=${OPA_NAMESPACE}`)
+    );
+
+    if (deleteResult && deleteResult.code === 0) {
+        clusterExplorer.refresh();
+        await vscode.window.showInformationMessage(`Deleted config map ${policy.metadata.name}`);
+    } else {
+        const reason = deleteResult ? deleteResult.stderr : 'unable to run kubectl';
+        await vscode.window.showErrorMessage(`Error deleteing config map ${policy.metadata.name}: ${reason}`);
+    }
+}
--- src/extension.ts
+++ src/extension.ts
@@ -3,12 +3,14 @@ import * as k8s from 'vscode-kubernetes-tools-api';
 import { install } from './commands/install';
 import { deployRego } from './commands/deploy-rego';
 import { unavailableMessage } from './utils/host';
-import { OPAPoliciesNodeContributor } from './ui/policies-node-contributor';
+import { PolicyBrowser } from './ui/policy-browser';
+import { deletePolicy } from './commands/delete-policy';
 
 export async function activate(context: vscode.ExtensionContext) {
     const disposables = [
         vscode.commands.registerCommand('opak8s.install', install),
         vscode.commands.registerTextEditorCommand('opak8s.deployRego', deployRego),
+        vscode.commands.registerCommand('opak8s.deletePolicy', deletePolicy),
     ];
 
     context.subscriptions.push(...disposables);
@@ -20,7 +22,7 @@ export async function activate(context: vscode.ExtensionContext) {
     } else if (!kubectl.available) {
         vscode.window.showWarningMessage(`Can't show OPA policies: ${unavailableMessage(kubectl.reason)}`);
     } else {
-        clusterExplorer.api.registerNodeContributor(new OPAPoliciesNodeContributor(kubectl.api, context));
+        clusterExplorer.api.registerNodeContributor(PolicyBrowser.create(kubectl.api, context));
     }
 }
 
